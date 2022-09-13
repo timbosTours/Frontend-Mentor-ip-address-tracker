1a65341f8e844ee3127613ed932f7f1ca5c083ce
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-09-14 07:19:22 +1000

    added leaflet map

---
+++ app.js
@@ -0,0 +1,13 @@
+// Create map with Leaflet.js(long, lat, zoom level)
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+// Load tiles onto map for map style
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// }).addTo(map);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
