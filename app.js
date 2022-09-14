f758b75929cd10cd4e6b8adfb5a1935df8a76f62
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-09-15 06:15:27 +1000

    node set up

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer(function (req, res) {
+    res.write('helloooo')
+    res.end()
+})
+
+server.listen(port, function (error) {
+    if (error) { 'whoops', error }
+    else {
+        console.log('server is listening on port' + port);
+    }
+}
+)
+
 // Create map with Leaflet.js(long, lat, zoom level)
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 
-// Load tiles onto map for map style
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '© OpenStreetMap'
-// }).addTo(map);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// Load tiles onto map for map styles
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
 }).addTo(map);
