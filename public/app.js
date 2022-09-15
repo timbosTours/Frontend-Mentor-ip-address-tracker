df4d8433209500ff64eae056379937ccc83540e2
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-09-16 07:38:19 +1000

    fixed small express bug

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./public'))
+app.use(express.static('./ip-address/tracker-master'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
