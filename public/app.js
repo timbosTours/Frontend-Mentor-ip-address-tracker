355fe663f22594e81e0dce67ceee58aa100807fd
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-09-26 07:12:45 +1000

    create function to load map data dynamically

--- public/app.js
+++ public/app.js
@@ -1,72 +1,80 @@
-async function getIpData() {
+async function getIpData(ipData) {
     // fetch data from node server
     const response = await fetch('http://localhost:5000/ipData');
     const data = await response.json();
     // convert json string into object
     var ipData = JSON.parse(data)
-    console.log(ipData);
-
+    console.log(ipData)
+    
     // grab ip address
     const ipAddress = ipData.ip;
     console.log(ipAddress)
-
-    // append with document fragment
+    
+    // append ip address, timezone, location and isp with document fragment
     const ipLi = document.getElementById('li-ipAddress');
     const docFrag1 = document.createDocumentFragment();
     let ipFrag1 = document.createElement('p');
     ipFrag1.textContent = ipAddress;
     docFrag1.appendChild(ipFrag1);
-
+    
     ipLi.appendChild(docFrag1)
     
     const location = ipData.location.region;
     console.log(location)
-
+    
     const locationLi = document.getElementById('li-location');
     const docFrag2 = document.createDocumentFragment();
     let ipFrag2 = document.createElement('p');
     ipFrag2.textContent = location;
     docFrag2.appendChild(ipFrag2);
-
+    
     locationLi.appendChild(docFrag2)
     
     const timezone = ipData.location.timezone;
     console.log(timezone)
-
+    
     const timezoneLi = document.getElementById('li-timezone');
     const docFrag3 = document.createDocumentFragment();
     let ipFrag3 = document.createElement('p');
     ipFrag3.textContent = timezone;
     docFrag3.appendChild(ipFrag3);
-
+    
     timezoneLi.appendChild(docFrag3)
     
     const isp = ipData.isp;
     console.log(isp)
-
+    
     const ispLi = document.getElementById('li-isp');
     const docFrag4 = document.createDocumentFragment();
     let ipFrag4 = document.createElement('p');
     ipFrag4.textContent = isp;
     docFrag4.appendChild(ipFrag4);
-
-    ispLi.appendChild(docFrag4)
-
     
-
-    
+    ispLi.appendChild(docFrag4);
 };
 
 getIpData();
 
 // // Create map with Leaflet.js(long, lat, zoom level)
 
+async function loadMap(){
+    // fetch data from node server
+    const response = await fetch('http://localhost:5000/ipData');
+    const data = await response.json();
+    // convert json string into object
+    var ipData = JSON.parse(data)
+    console.log(ipData)
 
-let map = leaflet.map('map').setView([51.505, -0.09], 13);
+
+    let map = leaflet.map('map').setView([51.505, -0.09], 13);
 
 
 // Load tiles onto map for map styles
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.geoJSON(ipData).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
-}).addTo(map);
+    }).addTo(map);
+}
+
+loadMap();
